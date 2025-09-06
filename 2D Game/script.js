b84b88e63a3ee474bbc045693b6356f9459f59e5
Aruna Rathnayaka <arathnayaka990@gmail.com>
2025-09-06 23:08:15 +0530

    Update script.js

--- 2D Game/script.js
+++ 2D Game/script.js
@@ -1,10 +1,35 @@
 // JavaScript Document
+var time = 100;
+var worker_1_id = 0;
+
 function start(event){
-	if (event.key=="Enter") {
-		alert("Run");
+	
+	if (event.code == "Enter" & worker_1_id == 0) { //Entrr ebuwoth haa worker id eka 0 unoth witharak run wenawa
+		startTimer();
 	}
 	
-	if (event.key==" ") {
-		alert("Attack");
+	if (event.code =="Space") {
+		alert("B");
 	}
 }
+
+function startTimer(){
+	
+	worker_1_id = setInterval(() => {
+		
+			time = time - 1; // time eken 1 adu wenawa
+	
+	 	if(time == 0){ // time eka " 0 " kiyala chek 
+	 	 	alert("Game Over"); 
+			window.location.reload(); // if eka reload wenna code eka
+	 	}
+
+		document.getElementById("time").innerHTML ="Your Remaining Time : " +time; //html wala text ekata java link kara	
+		
+	}, 1000);
+	
+
+}
+
+worker_name = setInterval(() =>{ /* wada karanna oni wada kotasa */ },1000 /* count wena welawa mili thathpara walin */ );
+
